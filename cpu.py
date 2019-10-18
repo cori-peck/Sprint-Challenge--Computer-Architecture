80c2fcd736815c542bfe03c33c33a433340be7aa
cori-peck <cori.peck@ymail.com>
2019-10-18 13:26:22 -0500

    MVP complete - prints 1 4 5

--- cpu.py
+++ cpu.py
@@ -10,6 +10,9 @@ PUSH = 0b01000101
 POP = 0b01000110
 HLT = 0b00000001
 CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
 
 class CPU:
     """Main CPU class."""
@@ -29,7 +32,10 @@ class CPU:
             PUSH: self.handlePUSH,
             POP: self.handlePOP,
             HLT: self.handleHLT,
-            CMP: self.handleCMP
+            CMP: self.handleCMP,
+            JMP: self.handleJMP,
+            JEQ: self.handleJEQ,
+            JNE: self.handleJNE
         }
 
     def handleLDI(self, ir, reg, val):
@@ -61,12 +67,33 @@ class CPU:
 
     def handleHLT(self):
         self.running = False
+        sys.exit()
 
     def handleCMP(self, ir, reg1, reg2):
         bit_operands = (ir & 0b11000000) >> 6
         #instruction handles by the ALU
         self.alu("CMP", reg1, reg2)
         self.pc += bit_operands + 1
+
+    def handleJMP(self, ir, regloc, op2):
+        #Set the PC to the address stored in the given register
+        self.pc = self.reg[regloc]
+
+    def handleJEQ(self, ir, regloc, op2):
+        #If equal flag is set (true), jump to the address stored in the given register
+        if (self.fl & 0b00000001) == 1:
+            self.pc = self.reg[regloc]
+        else:
+            bit_operands = (ir & 0b11000000) >> 6
+            self.pc += bit_operands + 1
+
+    def handleJNE(self, ir, regloc, op2):
+        #If E flag is clear (false, 0), jump to the address stored in the given register
+        if (self.fl & 0b00000001) == 0:
+            self.pc = self.reg[regloc]
+        else:
+            bit_operands = (ir & 0b11000000) >> 6
+            self.pc += bit_operands + 1
 
     def load(self):
         """Load a program into memory."""
@@ -116,10 +143,10 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        if op == "MUL":
+        elif op == "MUL":
             product = self.reg[reg_a] * self.reg[reg_b]
             self.reg[reg_a] = product
-        if op == "CMP":
+        elif op == "CMP": #0b00000LGE
             if self.reg[reg_a] == self.reg[reg_b]:
                 #if they are equal, set the Equal E flag to 1 otherwise set it to 0
                 self.fl = (self.fl | 0b00000001)
@@ -129,14 +156,14 @@ class CPU:
             if self.reg[reg_a] < self.reg[reg_b]:
                 #if registerA is < registerB, set the less-than L flag to 1 otherwise set to 0
                 self.fl = (self.fl | 0b00000100)
-            else:
-                self.fl(self.fl & 0b11111011)
+            #else:
+                #self.fl(self.fl & 0b11111011)
 
             if self.reg[reg_a] > self.reg[reg_b]:
                 #if registerA is > registerB, set the greater-than G flag to 1 otherwise set it to 0
                 self.fl = (self.fl | 0b00000010)
-            else:
-                self.fl = (self.fl & 0b11111101)
+            #else:
+                #self.fl = (self.fl & 0b11111101)
         else:
             raise Exception("Unsupported ALU operation")
 
